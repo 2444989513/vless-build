bc88e60391da81c00e671ae6a8fb4e8c71ee13e3
charlieethan <charlieethan.hk@gmail.com>
2020-09-26 10:53:21 +0800

    update

---
+++ src/v2ray.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+PLATFORM=$1
+if [ -z "$PLATFORM" ]; then
+    ARCH="amd64"
+else
+    case "$PLATFORM" in
+        linux/386)
+            ARCH="386"
+            ;;
+        linux/amd64)
+            ARCH="amd64"
+            ;;
+        linux/arm/v6)
+            ARCH="arm32-v6"
+            ;;
+        linux/arm/v7)
+            ARCH="arm32-v7"
+            ;;
+        linux/arm64|linux/arm64/v8)
+            ARCH="arm64"
+            ;;
+        linux/ppc64le)
+            ARCH="ppc64le"
+            ;;
+        linux/s390x)
+            ARCH="s390x"
+            ;;
+        *)
+            ARCH=""
+            ;;
+    esac
+fi
+[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
+# Download binary file
+V2RAY_FILE="v2ray-linux-${ARCH}.zip"
+
+echo "Downloading binary file: ${V2RAY_FILE}"
+wget -O $PWD/v2ray.zip https://https://github.com/charlieethan/vless-build/releases/latest/download/${V2RAY_FILE}.zip > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download binary file: ${V2RAY_FILE}" && exit 1
+fi
+echo "Download binary file: ${V2RAY_FILE} completed"
+
+echo "Prepare to use"
+unzip v2ray.zip && rm -rf v2ray.zip
+mv v2ray /usr/bin/v2ray && mv v2ctl /usr/bin/v2ctl
+
+chmod +x /usr/bin/v2ray
+chmod +x /usr/bin/v2ctl
